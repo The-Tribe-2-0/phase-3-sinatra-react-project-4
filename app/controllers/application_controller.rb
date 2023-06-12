1b7b1830c0df49061356572d28b01f8b6b3b1510
Mwanzia24 <mwanzaitom19@gmail.com>
2023-06-12 14:54:49 -0400

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,19 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-
   # GET /
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
-
   # GET /users
   get "/users" do
     users = User.all
     users.to_json
   end
-
   # GET /users/:id
   get "/users/:id" do
     user = User.find(params[:id])
     user.to_json
   end
-
   # POST /users
   post "/users" do
     user = User.new(JSON.parse(request.body.read))
@@ -29,7 +25,6 @@ class ApplicationController < Sinatra::Base
       { error: "Failed to create user" }.to_json
     end
   end
-
   # PUT /users/:id
   put "/users/:id" do
     user = User.find(params[:id])
@@ -40,26 +35,22 @@ class ApplicationController < Sinatra::Base
       { error: "Failed to update user" }.to_json
     end
   end
-
   # DELETE /users/:id
   delete "/users/:id" do
     user = User.find(params[:id])
     user.destroy
     status 204
   end
-
   # GET /categories
   get "/categories" do
     categories = Category.all
     categories.to_json
   end
-
   # GET /categories/:id
   get "/categories/:id" do
     category = Category.find(params[:id])
     category.to_json
   end
-
   # POST /categories
   post "/categories" do
     category = Category.new(JSON.parse(request.body.read))
@@ -71,7 +62,6 @@ class ApplicationController < Sinatra::Base
       { error: "Failed to create category" }.to_json
     end
   end
-
   # PUT /categories/:id
   put "/categories/:id" do
     category = Category.find(params[:id])
@@ -82,26 +72,23 @@ class ApplicationController < Sinatra::Base
       { error: "Failed to update category" }.to_json
     end
   end
-
   # DELETE /categories/:id
   delete "/categories/:id" do
     category = Category.find(params[:id])
     category.destroy
     status 204
   end
-
   # GET /drinks
-  get "/drinks" do
-    drinks = Drink.all
-    drinks.to_json
-  end
-
+ # GET /drinks
+get "/drinks" do
+  drinks = Drink.includes(:category).all
+  drinks.to_json(include: :category)
+end
   # GET /drinks/:id
   get "/drinks/:id" do
     drink = Drink.find(params[:id])
     drink.to_json
   end
-
   # POST /drinks
   post "/drinks" do
     drink = Drink.new(JSON.parse(request.body.read))
@@ -113,7 +100,6 @@ class ApplicationController < Sinatra::Base
       { error: "Failed to create drink" }.to_json
     end
   end
-
   # PUT /drinks/:id
   put "/drinks/:id" do
     drink = Drink.find(params[:id])
@@ -124,30 +110,61 @@ class ApplicationController < Sinatra::Base
       { error: "Failed to update drink" }.to_json
     end
   end
-
   # DELETE /drinks/:id
   delete "/drinks/:id" do
     drink = Drink.find(params[:id])
     drink.destroy
     status 204
   end
-
   # GET /drinks/search
   get "/drinks/search" do
     search_term = params[:q]
     drinks = Drink.where("name LIKE ?", "%#{search_term}%")
     drinks.to_json
   end
-
   # GET /brands
   get "/brands" do
     brands = Drink.select(:brand).distinct
     brands.to_json
   end
-
   # GET /brands/:brand
   get "/brands/:brand" do
     drinks = Drink.where(brand: params[:brand])
     drinks.to_json
   end
+  # POST /users
+post "/users" do
+  user_params = JSON.parse(request.body.read)
+  user = User.new(user_params)
+  if user.save
+    status 201
+    user.to_json
+  else
+    status 422
+    { error: "Failed to create user" }.to_json
+  end
+end
+post '/login' do
+  puts "Received login request: #{params}"
+  email = params[:email]
+  user = User.find_by(email: email)
+  if user
+    # User found
+    # Perform your custom authentication here based on email
+    # Assuming you have a custom authentication method called 'authenticate_by_email'
+    if user.authenticate_by_email(email)
+      # Authentication successful
+      # You can generate and return a token or session information here
+      { message: 'Login successful' }.to_json
+    else
+      # Authentication failed
+      status 401
+      { error: 'Authentication failed' }.to_json
+    end
+  else
+    # User not found
+    status 404
+    { error: 'User not found' }.to_json
+  end
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
-  has_many :drinks
+  # ... your other model code ...
+  def authenticate_by_email(email)
+    # Perform your custom authentication logic here
+    # For example, compare the email with the stored email in the user record
+    # Assuming you have an 'email' attribute in the User model
+    self.email == email
+  end
 end
+
+
+
+
+
+
+
